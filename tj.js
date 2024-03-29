1cb080df495982f67b342bdecece57702505098f
whuanle <1586052146@qq.com>
2024-03-29 08:50:04 +0800

    1

--- tj.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?6c87b1ecfaff402a1115ac30b476ecc9";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
